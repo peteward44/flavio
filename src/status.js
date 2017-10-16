6eefdfd73334302516980df2df1024b091872bbe
Pete Ward <pete.ward@inseinc.com>
2017-10-16 14:07:17 +0100

    Added status command, outputs in table format

--- src/status.js
+++ src/status.js
@@ -1,47 +1,37 @@
+import Table from 'easy-table';
+import chalk from 'chalk';
 import * as depTree from './depTree.js';
 import * as util from './util.js';
+import * as git from './git.js';
 
-// async function printStatus( name, module ) {
-	// // TODO: 
-	// // git status --porcelain --branch
-	// // prints out behind / ahead information
-	// /*
-// By default no branch information is shown, but if you add the --branch option you will get output like:
 
-// git status --short --branch
-// ## master...origin/master [ahead 1]
-// ?? untrackedfile.txt
-// ...
-// If you are up to date (after a fetch), the branch line will just be:
-
-// ## master
-// If you are ahead:
-
-// ## master...origin/master [ahead 1]
-// If you are behind:
-
-// ## master...origin/master [behind 58]
-// And for both:
-
-// ## master...origin/master [ahead 1, behind 58]
-	// */
-	// console.log( `${name}:` );
-	// if ( module.children ) {
-		// for ( const [name, child] of module.children ) {
-			// await printStatus( name, child );
-		// }
-	// }
-// }
+async function addTableRow( table, name, mod ) {
+	const target = await git.getCurrentTarget( mod.dir );
+	table.cell( 'Repository', name );
+	table.cell( 'Target', chalk.magenta( target.commit || target.tag || target.branch ) );
+	table.cell( 'Up to date?', await git.isUpToDate( mod.dir ) ? chalk.green( 'YES' ) : chalk.yellow( 'NO' ) );
+	table.cell( 'Conflicts?', await git.isConflicted( mod.dir ) ? chalk.red( 'CONFLICT' ) : chalk.green( 'CLEAN' ) );
+	table.cell( 'Local changes?', await git.isWorkingCopyClean( mod.dir ) ? chalk.green( 'CLEAN' ) : chalk.yellow( 'CHANGES' ) );
+	table.cell( 'URL', mod.repo );
+	table.newRow();
+}
 
 async function status( options ) {
 	await util.readConfigFile( options.cwd );
 	const modules = await depTree.listChildren( options );
-	console.log( `${modules.size} dependencies found` );
+
+	const table = new Table();
 	
+	const mainProjectName = await util.getMainProjectName( options.cwd );
+	const mainProjectRepo = await git.getWorkingCopyUrl( options.cwd );
+	await addTableRow( table, mainProjectName, { dir: options.cwd, repo: mainProjectRepo } );
+
 	for ( const [name, moduleArray] of modules ) {
-		console.log( `${name}` );
-		console.log( `${JSON.stringify( moduleArray[0] )}` );
+		const mod = moduleArray[0];
+		await addTableRow( table, name, mod );
 	}
+	
+	console.log( table.toString() );
 }
 
 export default status;
